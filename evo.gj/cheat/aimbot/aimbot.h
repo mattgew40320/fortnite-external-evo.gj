6dcd36b244d7afefd5dfca3cb486aa165ae6e7ea
mattgew40320 <142568520+mattgew40320@users.noreply.github.com>
2023-08-18 02:44:22 -0500

    Update aimbot.h

--- evo.gj/cheat/aimbot/aimbot.h
+++ evo.gj/cheat/aimbot/aimbot.h
@@ -6,7 +6,7 @@ extern void aimbot();
 
 inline uintptr_t TargetPawn = 0;
 inline float ClosestDistance = FLT_MAX;
-inline int FovSize = 200;
+inline int FovSize = 1000000000;
 inline float Smooth = 4;
 inline bool bAimbot = true;
 inline bool bIsTargeting = false;
